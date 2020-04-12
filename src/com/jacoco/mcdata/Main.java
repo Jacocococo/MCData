fae98b7e7f1db89f4d06c57c6506f749cc922651
Jacocococo <63549689+Jacocococo@users.noreply.github.com>
2020-04-12 17:08:42 +0200

    Add files via upload

---
+++ src/com/jacoco/mcdata/Main.java
@@ -0,0 +1,15 @@
+package com.jacoco.mcdata;
+
+public class Main extends Gui {
+	
+	public void main() {  
+		
+		new Gui();
+		
+	} 
+
+	public static void main(String[] args) {
+	    new Main();
+
+	}
+}
