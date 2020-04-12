dc872b3cc20bd37a2db36ec89221eef99ca9a114
Jacocococo <63549689+Jacocococo@users.noreply.github.com>
2020-04-12 17:08:42 +0200

    Initial setup

---
+++ src/com/jacoco/mcdata/Strings.java
@@ -0,0 +1,9 @@
+package com.jacoco.mcdata;
+
+public class Strings {
+
+	public static final String name = "MCData";
+	public static final String close = "Close";
+	public static final String file = "Choose Folder";
+	
+}
