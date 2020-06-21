36ad7a5cd575f86c49c8d144f66eda45f09931da
Jacocococo <63549689+Jacocococo@users.noreply.github.com>
2020-06-21 12:27:50 +0200

    Delete Strings.java

--- src/com/jacoco/mcdata/Strings.java
+++
@@ -1,9 +0,0 @@
-package com.jacoco.mcdata;
-
-public class Strings {
-
-	public static final String name = "MCData";
-	public static final String close = "Close";
-	public static final String file = "Choose Folder";
-	
-}
