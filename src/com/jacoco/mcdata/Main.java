652b16d1283b91d73db0b8c4d47b0a739418b697
Jacocococo <63549689+Jacocococo@users.noreply.github.com>
2020-06-21 12:28:50 +0200

    Add files via upload

---
+++ src/com/jacoco/mcdata/Main.java
@@ -0,0 +1,11 @@
+package com.jacoco.mcdata;
+
+import com.jacoco.mcdata.files.Config;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		new Config();
+		
+	}
+}
