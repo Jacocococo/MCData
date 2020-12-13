b6eda692636ee29f57670bf3fcdcadc2111d8049
Jacocococo <63549689+Jacocococo@users.noreply.github.com>
2020-12-13 14:12:10 +0100

    Update Main.java

--- src/com/jacoco/mcdata/Main.java
+++ src/com/jacoco/mcdata/Main.java
@@ -1,11 +1,33 @@
 package com.jacoco.mcdata;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import com.jacoco.mcdata.files.Config;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		new Config();
-		
+	private static Path tmpDir;
+	private static Config cfg;
+	private static Gui gui;
+	
+	public static void main(String[] args) throws IOException {
+		tmpDir = Files.createTempDirectory("MCData_");
+		tmpDir.toFile().deleteOnExit();
+		cfg = new Config();
+		gui = new Gui(cfg.getTheme(), cfg);
+	}
+
+	public static Path getTmpDir() {
+		return tmpDir;
+	}
+
+	public static Config getCfg() {
+		return cfg;
+	}
+
+	public static Gui getGui() {
+		return gui;
 	}
 }
